be5e7bfa17ba3314f70cebf0d8b6ffa3b34a7db0
jyopa <jyopa@LAPTOP-BISOCHBF>
2021-03-24 05:25:19 -0500

    Nuevo controlador

--- src/java/controllers/ControladorProducto.java
+++ src/java/controllers/ControladorProducto.java
@@ -1,25 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package controllers;
 
 import Constructor.Con_productos;
 import Modelos.Modelo_Productos;
 
-/**
- *
- * @author Personal
- */
+
 public class ControladorProducto {
-    
     public String getProductos(){
-        Modelos.Modelo_Productos mp=new Modelo_Productos();
-        String  htmlcode ="";
+        String htmdoc="";
+        Modelos.Modelo_Productos mod=new Modelo_Productos();
         
-        for(Con_productos con: mp.consultarusuarios()){
-            htmlcode  +="<div class=\"card\" style=\"\">\n" +
+        for(Con_productos con: mod.consultarusuarios()){
+            htmdoc+="<div class=\"card\" style=\"\">\n" +
 "                                <img src=\""+con.getImg()+"\" class=\"card-img-top\" alt=\"...\">\n" +
 "                                <h1 class=\"card-header\">"+con.getNombre_pro()+"</h1>\n" +
 "                                    <div class=\"card-body text-center\">\n" +
@@ -27,14 +18,14 @@ public class ControladorProducto {
 "                                      <p class=\"card-footer\">"+con.getDescripcion()+"</p>\n" +
 "                                      <div>\n" +
 "                                          <a href=\"producto-detalles.jsp?id="+con.getCod_pro()+"\" class=\"btn btn-outline-info\">Ver detalles</a>\n" +
-"                                      </div>\n" +
-"                                      \n" +
-"                         </div>\n";                                                          
+"                                      </div>\n" +                 
+"                                </div>\n"+
+"                                </div>\n";
         }
         
-        return htmlcode;
+        
+        return htmdoc;
     }
-    
     public Con_productos getproducto(String id){
         
         return new Modelos.Modelo_Productos().listar(id);
